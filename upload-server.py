a9368ee0debe991ccd2241b2bf2df8601c544e47
Aptimex <adammerrill13@gmail.com>
2024-08-31 18:05:47 -0600

    better directory handling

--- upload-server.py
+++ upload-server.py
@@ -65,26 +65,32 @@ class CustomHTTPRequestHandler(SimpleHTTPRequestHandler):
         #reply_body = 'Saved "{}"\n'.format(filename)
         #self.wfile.write(reply_body.encode('utf-8'))
         
-        print(f"File of size {file_length} saved to {filePath}")
+        print(f"File of size {file_length} bytes saved to {filePath}")
 
 def main(args):
+    # Make sure we're saving to a valid directory
     serveDir = Path(os.path.realpath(args.directory))
+    if not os.path.isdir(serveDir):
+        print(f"Path {serveDir} does not exist or is not a directory, exiting")
+        exit(1)
     os.chdir(serveDir)
-    print(f"Listening on {args.ip}:{args.port}")
-    print(f"Files will be save to {serveDir}")
     
+    # Display important server parameters being used
+    print(f"Listening on {args.ip}:{args.port}")
+    print(f"Files will be saved to {serveDir}")
     if args.secret:
         global SECRET
         SECRET = args.secret
         print(f"Uploads must include the 'secret={SECRET}' URL parameter")
     
+    # Start the server
     httpd = HTTPServer((args.ip, args.port), CustomHTTPRequestHandler)
     httpd.serve_forever()
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="HTTP server that accepts POST requests and saves them to a file. Best used with \"curl --data-binary '@myFile.txt' IP:PORT/myFile.txt\"")
     parser.add_argument("port", type=int, default=8123, nargs='?', help="Port to listen on")
-    parser.add_argument("directory", default="./", nargs='?', help="Directory to save files to")
+    parser.add_argument("-d", "--directory", default="./", help="Directory to save files to")
     parser.add_argument("-i", "--ip", default="0.0.0.0", help="IP to listen on")
     parser.add_argument("-s", "--secret", default=None, help="If specified, this value must be present as a URL 'secret' (case-sensitive) parameter for the server to accept the file. E.g. 1.2.3.4/file?secret=mySuperSecret")
     
